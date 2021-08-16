925641138e8751a2e25289c8bdeeaf3db19381c4
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2021-08-16 17:30:06 +0800

    Add files via upload

--- start_experiment.sh
+++ start_experiment.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# script results.txt
 for (( i=1; i<=8; i++ ))
 do
   # for (( j=1; j<=10; j++ ))
   # do
   docker-compose -f $i.yaml up -d
   sleep 260
-  # script $i$j.txt
-  docker logs experimenter
-  # exit
+  docker logs experimenter | tee $i.txt
   docker kill $(docker ps -q)
   sleep 10
   docker rm $(docker ps -a -q)
