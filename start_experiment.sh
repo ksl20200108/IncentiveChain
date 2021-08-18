39a19f1c64fba1216ec0d890e176753ebac97217
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2021-08-18 13:31:15 +0800

    Update start_experiment.sh

--- start_experiment.sh
+++ start_experiment.sh
@@ -3,7 +3,7 @@ for (( i=1; i<=8; i++ ))
 do
   for (( j=1; j<=10; j++ ))
   do
-    docker-compose -f $i.yaml up -d
+    docker-compose -f $i$j.yaml up -d
     sleep 200    # please leave enough time + 20 for creating containers
     docker logs experimenter &> $i$j.log &
     docker kill $(docker ps -q)
