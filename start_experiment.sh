4a059d8f683fb5b00e28be0c805556a75c9bc021
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2021-10-04 15:33:32 +0800

    Add files via upload

--- start_experiment.sh
+++ start_experiment.sh
@@ -3,12 +3,13 @@ for (( i=1; i<=8; i++ ))
 do
   for (( j=1; j<=10; j++ ))
   do
-    docker-compose -f $i$j.yaml up -d
-    sleep 240    # please leave enough time + 20 for creating containers
-    docker logs experimenter &> $i$j.log &
-    docker kill $(docker ps -q)
+    docker-compose -f $i$jstatic.yaml up -d    # new
+    docker stack    # new
+    sleep 240
+    docker logs experimenter &> $i$j.log &    # new
+    docker kill $(docker ps -q)    # new
     sleep 40
-    docker rm $(docker ps -a -q)
+    docker rm $(docker ps -a -q)    # new
     sleep 40
   done
 done
--- write_static_compose.py
+++ write_static_compose.py
@@ -5,7 +5,9 @@ def write_yaml(main_num):
     fee2 = ["", "FTETSIMfee21600", "FTETSIMfee24800", "FTETNSIMfee21600", "FTETNSIMfee24800",
             "CURRENTSIMfee21600", "CURRENTSIMfee24800", "CURRENTNSIMfee21600", "CURRENTNSIMfee24800"]
 
-    f = open(str(main_num) + ".yaml", "w")
+    """new"""
+    f = open(str(main_num) + "static.yaml", "w")
+
     f.write("version: '3'\n")
     f.write("\n")
     f.write('services:\n')
