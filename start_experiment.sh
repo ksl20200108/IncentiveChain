477861cde2d278d989d12babc7f5252698d48cf7
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2021-10-05 15:37:37 +0800

    Add files via upload

--- start_experiment.sh
+++ start_experiment.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+
+docker swarm init
+docker network create --driver overlay --subnet 192.168.0.0/16 --gateway 192.168.0.1 --attachable test
+
 for (( i=1; i<=8; i++ ))
 do
   for (( j=1; j<=10; j++ ))
   do
-    docker-compose -f $i$jstatic.yaml up -d    # new
-    docker stack    # new
+    docker stack deploy -c $i$jstatic.yaml test
+    docker stack deploy -c $i$jdynamic.yaml test
     sleep 240
-    docker logs experimenter &> $i$j.log &    # new
-    docker kill $(docker ps -q)    # new
+    docker service logs test_experimenter &> $i$j.log &
+    docker stack rm test
+    docker kill $(docker ps -q)
     sleep 40
-    docker rm $(docker ps -a -q)    # new
+    docker rm $(docker ps -a -q)
     sleep 40
   done
 done
