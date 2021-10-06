13f34b6660b0f3d48cd0a183f4e9dddd730efedc
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2021-10-06 09:46:00 +0800

    Add files via upload

--- test_experiment.sh
+++ test_experiment.sh
@@ -4,7 +4,8 @@ docker network create --driver overlay --subnet 192.168.0.0/16 --gateway 192.168
 
 docker stack deploy -c 11static.yaml test
 docker stack deploy -c 11dynamic.yaml test
-sleep 240
+sleep 15000
 docker service logs test_experimenter &> 11.log &
 docker stack rm test
+sleep 10000
 docker kill $(docker ps -q)
